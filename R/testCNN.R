a2bd0a48f1205aee1c2804537e110a393ce42559
xavier <xavier@gandillot.com>
2016-07-08 17:11:10 +0200

    test of CNN with convolution

---
+++ R/testCNN.R
@@ -0,0 +1,60 @@
+# test convolutions
+
+# see : https://github.com/dmlc/mxnet/issues/2138
+# see : http://tjo-en.hatenablog.com/entry/2016/03/30/233848
+
+require(mxnet)
+require(dplyr)
+require(tidyr)
+
+
+# define a model with 1 linear regression outputs
+data <-  mx.symbol.Variable('data')
+
+fc1  <- mx.symbol.Convolution(data = data,  kernel=c(2,2),num.filter=5)
+act1 <- mx.symbol.Activation(data = fc1, act_type="relu")
+pl1 <- mx.symbol.Pooling(data=act1, pool.type = "max", kernel=c(2,2), stride=c(2,2)  )
+
+ff <- mx.symbol.Flatten(data=pl1) ## needed after convolution steps ...
+
+fc2  <- mx.symbol.FullyConnected(data = ff,  num_hidden=1)
+net  <- mx.symbol.LinearRegressionOutput(data=fc2)
+
+
+# Create artificial data - careful : colMajor convention, not the usual rowMajor !!
+X <- array(1, c(96,96,1, 2140)) # we need 4 dimensions to enter the convolution layer !
+y <-array(0.5,c(2140))
+
+
+
+# create train/eval  subset for cross validation
+eval <- sample.int(length(y), 300) # save 300 values for evaluation
+# train
+# caution we loose the articficial 3rd dimension, 
+# moving from 4D 96x96x1x ... to 3D 96x96x ..
+train.X <- X[,,,-(eval)]
+train.y <- y[-eval]
+# eval
+eval.X <- X[,,,eval]
+eval.y <- y[eval]
+# Important : reshape the lost dimension
+dim(train.X) <- c(96,96,1,dim(train.X)[3])
+dim(eval.X) <- c(96,96,1,dim(eval.X)[3])
+
+
+# train model
+model <- mx.model.FeedForward.create (
+  X = train.X,
+  y = train.y,
+  eval.data = list(data=eval.X, label=eval.y),
+  eval.metric = mx.metric.rmse,
+  ctx  = mx.cpu(),
+  symbol = net,
+  num.round  = 4,
+  learning.rate  = 0.001,
+  momentum = 0.9,
+  wd = 0.00001,
+  initializer  = mx.init.Xavier(),
+  verbose = TRUE,
+  array.layout = "colMajor"
+)
